4f879fd928aae38cb9627e67478aac6420987b8b
Luis Torres <luis.torres@venteks.com>
2022-02-03 14:59:17 -0600

    adding custom color to theme

---
+++ ULALA/Themes/DefaultTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+
+namespace ULALA.Themes
+{
+    public partial class DefaultTheme : ResourceDictionary
+    {
+        public DefaultTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
