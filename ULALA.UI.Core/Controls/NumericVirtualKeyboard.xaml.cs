d690bdadbfcf9760864e48d63f10be9de37e0bb6
Luis Torres <luis.torres@venteks.com>
2022-02-08 18:51:08 -0600

    Numeric virtual keyboard control UI added

---
+++ ULALA.UI.Core/Controls/NumericVirtualKeyboard.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System;
+using Xamarin.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+
+namespace ULALA.UI.Core.Controls
+{
+    
+    public partial class NumericVirtualKeyboard : UserControl
+    {
+        public NumericVirtualKeyboard()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
