8109e22183e79510e83a0f709bb2d0ab6c2b8c5c
Diego Padilla <diego.padilla.dev@gmail.com>
2022-03-04 12:28:32 -0600

    Frist Commit New Branch

---
+++ ULALA.Services.Contracts/Zeus/IZeusException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULALA.Services.Contracts.Zeus
+{
+    public interface IZeusException
+    {
+        //Global exception catcher
+
+        void SendToLogger(Exception e);
+
+
+        
+    }
+}
